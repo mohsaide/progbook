025c8f30dc3245820deb034219961038643c8873
mohsadie <104079314+mohsadie@users.noreply.github.com>
2025-02-06 02:12:42 +0200

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,10 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, "/progbook")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "progbook_project.settings")
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works correctly!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+application = get_wsgi_application()
