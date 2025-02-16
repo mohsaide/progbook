bd22d2325451444ac18f3737bff54ed335a03c65
mohsadie <104079314+mohsadie@users.noreply.github.com>
2025-02-16 02:50:08 +0200

    push changes

--- progbook_project/asgi.py
+++ progbook_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'progbook_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('SETTINGS_FILE'))
 
 application = get_asgi_application()
