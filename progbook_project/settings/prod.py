60302ba07a6817a37e185a3e296aad92ed24b068
mohsadie <104079314+mohsadie@users.noreply.github.com>
2025-02-16 02:22:02 +0200

    commit

--- progbook_project/settings/prod.py
+++ progbook_project/settings/prod.py
@@ -2,4 +2,4 @@ from progbook_project.settings.base import *
 
 ALLOWED_HOSTS = ['progbook.app']
 
-STATIC_ROOT = '/home/progxywu/progbook/public_html/progbook_collected_statics'
+STATIC_ROOT = '/home/progxywu/public_html/progbook_collected_statics'
