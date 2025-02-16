d9f408cadb852054daa02bc7811cf8402e32c2c3
mohsadie <104079314+mohsadie@users.noreply.github.com>
2025-02-16 16:50:27 +0200

    push changes

--- progbook_landing/urls.py
+++ progbook_landing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = \
 [
-    path('' , views.landing_page , name = 'landing_page')
+    path('' , views.landing_page , name = 'home'),
+    path( 'service/<str:name>/' , views.service , name = 'service')
 ]
--- progbook_landing/views.py
+++ progbook_landing/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def landing_page(request):
-    return render(request, 'landing_page_index.html' )
+    return render(request, 'landing_page/index.html' )
+
+def service(request , name):
+    context = {'name' : str(name).replace('_' , ' ').title() }
+    return render(request, 'landing_page/service.html' , context )
