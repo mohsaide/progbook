661ea5ec3bce9275b605ae5075df70fa8503ae0f
mohsadie <104079314+mohsadie@users.noreply.github.com>
2025-02-16 17:29:56 +0200

    push changes

--- progbook_landing/urls.py
+++ progbook_landing/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = \
 [
     path('' , views.landing_page , name = 'home'),
-    path( 'service/<str:name>/' , views.service , name = 'service')
+    path( 'service/<str:name>/' , views.service , name = 'service'),
+    path( 'login/' , views.login , name = 'login'),
+    path('reset_password/', views.reset_password, name='reset_password')
 ]
--- progbook_landing/views.py
+++ progbook_landing/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def landing_page(request):
     return render(request, 'landing_page/index.html' )
 
+def login(request):
+    return render(request, 'landing_page/login.html' )
+
+def reset_password(request):
+    return render(request, 'landing_page/reset_password.html' )
+
 def service(request , name):
     context = {'name' : str(name).replace('_' , ' ').title() }
     return render(request, 'landing_page/service.html' , context )
